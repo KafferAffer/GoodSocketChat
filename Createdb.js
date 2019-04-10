2ed921f8283ccd4d29549548eb014f1ba92b2bbc
KafferAffer <46445011+KafferAffer@users.noreply.github.com>
2019-04-10 13:21:58 +0200

    repository

---
+++ Createdb.js
@@ -0,0 +1,74 @@
+var mysql = require('mysql');
+var promise = require('promise');
+//defines the connection to localhost
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+//connects to localhost
+con.connect(function(err) {
+if (err) throw err;
+    console.log("Database Connected!");
+});
+
+//creates database and tables if they dont exist
+
+
+    
+    //Creates Chromechat
+    var sql = "CREATE DATABASE IF NOT EXISTS ChromeChat";
+    
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log("Database created");
+    });
+    
+    //Creates User tables
+    var sql = "CREATE TABLE IF NOT EXISTS ChromeChat.USER (\
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,\
+        navn VARCHAR(30) NOT NULL,\
+        password VARCHAR(512) NOT NULL\
+        )";
+    
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log("User table created");
+    });
+    
+    //Creates Chat tables
+    var sql = 'CREATE TABLE IF NOT EXISTS ChromeChat.CHAT (\
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,\
+        navn VARCHAR(30) NOT NULL,\
+        owner_id INT(6) UNSIGNED,\
+        FOREIGN KEY (owner_id) REFERENCES ChromeChat.USER(id)\
+        )';
+    
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log("Chat table created");
+    });
+    
+    //Creates Member tables
+    var sql = "CREATE TABLE IF NOT EXISTS ChromeChat.MEMBER (\
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,\
+        user_id INT(6) UNSIGNED,\
+        chat_id INT(6) UNSIGNED,\
+        FOREIGN KEY (user_id) REFERENCES ChromeChat.USER(id),\
+        FOREIGN KEY (chat_id) REFERENCES ChromeChat.CHAT(id)\
+        )";
+    
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log("Member table created");
+    });
+    
+     //Creates Message tables
+    var sql = "CREATE TABLE IF NOT EXISTS  ChromeChat.MESSAGE (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, user_id INT(6) UNSIGNED, chat_id INT(6) UNSIGNED, message varchar(255), FOREIGN KEY (user_id) REFERENCES ChromeChat.USER(id), FOREIGN KEY (chat_id) REFERENCES ChromeChat.CHAT(id))";
+    
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log("Message table created");
+    });
+    
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+console.log("server is running");
+app.use(express.static('public'));
+
+//SQL STUFF
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+//connects to localhost
+con.connect(function(err) {
+if (err) throw err;
+    console.log("Database Connected!");
+});
+
+//SOCKET STUFF
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection',newConnection);
+
+function newConnection(socket){
+    socket.on('login',function(username,password){
+        console.log('navn: ' + username);
+        console.log('password: ' + password);
+        var sql = "SELECT * FROM ChromeChat.USER WHERE navn = '"+ username +"' AND password = '"+password+"'";
+        con.query(sql, function (err, result) {
+            if (err) reject(err);
+            if(result.length>0){
+                console.log("welcome " + username);
+                socket.emit('login',true);
+            }else{
+                socket.emit('errormsg',"user and password doesnt exist");
+            }
+        });
+    });
+    console.log(socket.id);
+}
+
+
+
